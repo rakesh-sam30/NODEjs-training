fec5cd81440b4c133629c349a3c98a36f1c1fb6a
samyog <080bct070@ioepc.edu.np>
2024-07-09 09:35:48 +0545

    new commit

--- app.js
+++ app.js
@@ -3,26 +3,34 @@ const express = require("express");
 const connectToDb = require("./database/databaseConnection");
 const Blog = require("./model/blogmodel");
 const app = express();
+const {multer,storage}= require('./middleware/multerConfig');
+const upload= multer({storage: storage})
 connectToDb()
 // code that is used everyday in node
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 // till here
 app.set("view engine", "ejs");
-app.get("/createblog", (req, res) => {
-    res.render("index");
+app.get("/createblog", async (req, res) => {
+    const blogs= await Blog.find()
+    res.render("homepage",{blogs});
 });
-app.post("/createblog", async (req,res)=>{
+app.post("/createblog",upload.single('image') ,async (req,res)=>{
+    const fileName=req.file.filename;
     const {title,subtitle,description}=req.body;
+    console.log(title,subtitle,description);
 
     const blog=await Blog.create({
         title,
         subtitle,
-        description
+        description,
+        image:fileName
     });
     console.log(req.body);
-    res.send(blog)
+    res.send("Blog Sucessfully posted")
 });
+
+    app.use(express.static("./views"))
 app.listen(3000,()=>{
     console.log("connection sucessfully");
 });
--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
  
 async function connectToDb(){
-    await mongoose.connect("mongodb+srv://080bct070:w4goSoDJQNvzxuNA@cluster0.g4dp2g7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb://localhost:27017/new");
     console.log("Connected");
 }
 module.exports=connectToDb;
